f5e3875fb6373b1a9c547fd8dc1e6428ab2cc68d
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-12-21 09:39:12 +0530

    firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCL20yL9JbpIXFRLaDjh-dH5BTY7YdrXLw",
+    authDomain: "netflix-clone-f58a7.firebaseapp.com",
+    projectId: "netflix-clone-f58a7",
+    storageBucket: "netflix-clone-f58a7.appspot.com",
+    messagingSenderId: "1004053514057",
+    appId: "1:1004053514057:web:83ba9acb3ddb1bbdb4fc98"
+};
