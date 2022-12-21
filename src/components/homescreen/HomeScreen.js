e27e5566ddaef0c2524d43f6fb5612e42e8e421e
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-12-21 12:53:19 +0530

    building row component

--- src/components/homescreen/HomeScreen.js
+++ src/components/homescreen/HomeScreen.js
@@ -2,16 +2,26 @@ import React from 'react';
 import "../homescreen/HomeScreen.css"
 import NavBar from '../navbar/NavBar';
 import Banner from '../banner/Banner';
+import Row from '../row/Row';
+import requests from '../../api/Requests';
 
 function HomeScreen() {
-  return <div className='homeScreen'>
-    <NavBar /> 
+  return (
+    <div className='homeScreen'>
+      <NavBar />
 
-    <Banner />
-    
-    {/* Row */}
-  
-  </div>
+      <Banner />
+
+      < Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      < Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      < Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      < Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      < Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      < Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      < Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      < Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
 export default HomeScreen;
