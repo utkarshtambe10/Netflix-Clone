e1975a696f60e85f9c16e4fd9bab9683313dcb29
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-12-21 10:56:23 +0530

    building navbar component

---
+++ src/components/homescreen/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../homescreen/HomeScreen.css"
+import NavBar from '../navbar/NavBar';
+
+function HomeScreen() {
+  return <div className='homeScreen'>
+    <NavBar /> 
+
+    <Banner />
+    
+    {/* Row */}
+  
+  </div>
+}
+
+export default HomeScreen;
